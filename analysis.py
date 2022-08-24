a0c21ad216824b2f919220e5bd0c3362cf04b259
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-08-24 12:54:24 +0900

    修正

--- analysis.py
+++ analysis.py
@@ -7,12 +7,15 @@ def word_extraction(video_data, nelogd = True):
     result = []
     ban_detail = ['副詞可能','数','非自立','代名詞','接尾']
     target_speech = ["名詞","形容詞","形容動詞"]
+    ban_word = morpheme(clean_text(video_data["title"]),nelogd = nelogd)
     for comment in video_data['comment']:
         comment = comment.replace('\n','')
         comment = clean_text(comment)
         word_dict = morpheme(comment, kind = True, nelogd = nelogd)
         for w,s in word_dict.items():
-            if s['speech'] in target_speech and s['reading'] != '*' and s['detail_speech'][0] not in ban_detail:
+            if s['speech'] in target_speech and s['reading'] != '*' and \
+               s['detail_speech'][0] not in ban_detail and s["endform"] not in ban_word \
+               and w not in ban_word:
                 result.append(s['endform'])
     return ' '.join(result)
 
