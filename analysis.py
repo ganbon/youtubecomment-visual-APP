67c1ab167114d69b40be5db8eb305c7723db4ef0
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-08-31 01:07:19 +0900

    自作ライブラリの変更点を反映

--- analysis.py
+++ analysis.py
@@ -11,7 +11,7 @@ def word_extraction(video_data, nelogd = True):
     for comment in video_data['comment']:
         comment = comment.replace('\n','')
         comment = clean_text(comment)
-        word_dict = morpheme(comment, kind = True, nelogd = nelogd)
+        word_dict, sentence = morpheme(comment, kind = True, nelogd = nelogd)
         for w,s in word_dict.items():
             if s['speech'] in target_speech and s['reading'] != '*' and \
                s['detail_speech'][0] not in ban_detail and s["endform"] not in ban_word \
